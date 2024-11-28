6b35dac7994747dbacce86f6e0ff6f535f8d2cdc
OscarJEB <burgeo1@macstudent.com.au>
2024-11-29 08:50:33 +1100

    Completed indoor and playback problems, both from scratch only using cs50.ai to find the .replace() method.

---
+++ indoor.py
@@ -0,0 +1,4 @@
+#Ask user, convert their input to lower case without unnecessary spaces
+a = input("WHAT DO YOU WANT IN LOWER CASE!?!?! ").lower().strip()
+#print lower case input
+print(a)
---
+++ playback.py
@@ -0,0 +1,2 @@
+f = input("What do you want to be dramatic? ").strip().replace(" ", "...")
+print(f)
